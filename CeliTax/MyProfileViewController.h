3ed95b89154fa720ca57d61a63df3e6ada9c9d8c
Leon Chen <lchen@lchenmb.local>
2015-07-30 18:57:18 -0400

    WIP

---
+++ CeliTax/MyProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyProfileViewController.h
+//  CeliTax
+//
+//  Created by Leon Chen on 2015-07-24.
+//  Copyright (c) 2015 CraveNSave. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface MyProfileViewController : BaseViewController
+
+@end
